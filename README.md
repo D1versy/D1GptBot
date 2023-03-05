9ee81d601daeb5b6869ae74a901de2566e12e45b
if <d5versy@gmail.com>
2023-03-05 14:12:53 +0200

    Bot instructions was sadded

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# D1GptBot
+# D1TikTokBot
+
+ A Telegram Bot to Download TikTok Videos
+
+## Configuring Environments 
+
+- add your token in env key= `BOT_TOKEN` : generate it from [BotFather](https://t.me/BotFather)
+- add your openAi  token in env key= `OPENAI_KEY` : generate it from (https://platform.openai.com/account/api-keys)
+- install [python3](https://www.python.org/downloads/)
+- install requirements `pip install -r requirements.txt`
+- Clone the repo
+- run `main.py`
+
+## Description
+
+I am a software engineer. I develop software applications using a variety of programming languages, databases and frameworks. I develop applications for the web, desktop, mobile devices and other platforms. I also write code to integrate software applications with other systems and databases. Additionally, I troubleshoot software issues, optimize applications for performance and security, and provide technical support.
+
+🔑 BOT Commands : `/start` , `/help` , `/about`
+
+## Troubleshooting
+
+python not found?
+1. use `python3` in console
+
+pip doesn't work?
+1. download pip scrypt
+```commandline
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+```
+2. run it `$ python3 get-pip.py`
---
+++ main.py
@@ -0,0 +1,70 @@
+import telegram
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+)
+import openai
+import os
+
+# Set up your OpenAI API credentials
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+openai.api_key = os.getenv("OPENAI_KEY")
+
+
+def start_handler(update, context):
+    update.message.reply_text("Hello! I'm a bot that can answer questions. Just send me a question and I'll do my best to answer it.\nMore details: /help")
+
+
+def about_handler(update, context):
+    update.message.reply_text("I am a software engineer. I develop software applications using a variety of programming languages, databases and frameworks. I develop applications for the web, desktop, mobile devices and other platforms. I also write code to integrate software applications with other systems and databases. Additionally, I troubleshoot software issues, optimize applications for performance and security, and provide technical support.")
+
+
+def help_handler(update, context):
+    update.message.reply_text('BOT Commands : /start , /about\n"Availables D1 bots:\nhttps://t.me/D1VideoBot\nhttps://t.me/D1GptBot\nhttps://t.me/D1TikTokBot')
+
+
+def answer_question(message, update, context):
+    # Get the user's question from the message
+    message = update.message
+    status_msg = message.reply_text(message, "🚀 Processing ... \n it might take some time :)")
+
+    question = message.text.strip()
+
+    # Call the OpenAI API to get an answer
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"Q: {question}\nA:",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    # Send the answer back to the user
+    answer = response.choices[0].text.strip()
+    status_msg.edit_text(message, answer)
+
+def incoming_message_action(update, context):
+    message = update.message
+    context.dispatcher.run_async(answer_question, str(message.text), update, context)
+
+
+
+def main() -> None:
+    """Run the bot."""
+    updater = Updater(BOT_TOKEN)
+    dispatcher = updater.dispatcher
+
+    # Commands Listning
+    dispatcher.add_handler(CommandHandler('start', start_handler, run_async=True))
+    dispatcher.add_handler(CommandHandler('about', about_handler, run_async=True))
+    dispatcher.add_handler(CommandHandler('help', help_handler, run_async=True))
+
+    # Message Incoming Action
+    dispatcher.add_handler(MessageHandler(incoming_message_action,run_async=True))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
