0c35a89d59b4642545cc983f4b425d053065c23e
if <d5versy@gmail.com>
2023-04-20 15:25:19 +0300

    Update the bot to remember chat histiry for 10 min

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import logging
+import os
+import time
+from datetime import datetime, timedelta
+
+import openai
 import telegram
 from telegram.ext import (
     Updater,
@@ -6,8 +11,6 @@ from telegram.ext import (
     MessageHandler,
     Filters
 )
-import openai
-import os
 
 # Set up your OpenAI API credentials
 GPT_BOT_TOKEN = os.getenv("GPT_BOT_TOKEN")
@@ -16,53 +19,84 @@ openai.api_key = os.getenv("OPENAI_KEY")
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+
+class UserHistory:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.history = []
+        self.last_interaction = datetime.now()
+
+    def add_message(self, sender, message):
+        self.history.append({"sender": sender, "message": message})
+        self.last_interaction = datetime.now()
+
+    def get_conversation(self):
+        return [message["message"] for message in self.history]
+
+    def is_expired(self, expiration_time=10):
+        return datetime.now() - self.last_interaction > timedelta(minutes=expiration_time)
+
+
+user_histories = {}
+
+
 def start_handler(update, context):
-    update.message.reply_text("Hello! I'm a bot that can answer questions. Just send me a question and I'll do my best to answer it.\nMore details: /help")
+    update.message.reply_text(
+        "Hello! I'm a bot that can answer questions. Just send me a question and I'll do my best to answer it.\nMore details: /help")
 
 
 def about_handler(update, context):
-    update.message.reply_text("I am a software engineer. I develop software applications using a variety of programming languages, databases and frameworks. I develop applications for the web, desktop, mobile devices and other platforms. I also write code to integrate software applications with other systems and databases. Additionally, I troubleshoot software issues, optimize applications for performance and security, and provide technical support.")
+    update.message.reply_text(
+        "I am a software engineer. I develop software applications using a variety of programming languages, databases and frameworks. I develop applications for the web, desktop, mobile devices and other platforms. I also write code to integrate software applications with other systems and databases. Additionally, I troubleshoot software issues, optimize applications for performance and security, and provide technical support.")
 
 
 def help_handler(update, context):
-    update.message.reply_text('BOT Commands : /start , /about\n"Available D1 bots:\nhttps://t.me/D1VideoBot\nhttps://t.me/D1GptBot\nhttps://t.me/D1TikTokBot')
+    update.message.reply_text(
+        'BOT Commands : /start , /about\nAvailable D1 bots:\nhttps://t.me/D1VideoBot\nhttps://t.me/D1GptBot\nhttps://t.me/D1TikTokBot')
 
 
-# Define a handler function for all other messages
+def chat_gpt_request(prompt, max_tokens=150):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=max_tokens,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    return response.choices[0].text.strip()
+
+
+def handle_user_message(user_id, message, user_histories):
+    if user_id not in user_histories or user_histories[user_id].is_expired():
+        user_histories[user_id] = UserHistory(user_id)
+
+    user_history = user_histories[user_id]
+
+    user_history.add_message("user", message)
+
+    conversation = "\n".join(user_history.get_conversation())
+    prompt = f"{conversation}\nbot:"
+
+    response = chat_gpt_request(prompt)
+    user_history.add_message("bot", response)
+    return response
+
+
 def answer_question(update, context):
     message = update.message
-    file = open(f'./log.txt', "a+")
-    file.write(f"{message.from_user.username} => {message.text}")
-    file.write("\n")
-    file.close()
-    status_msg = message.reply_text("🚀 Processing ... \n it might take some time :)")
+    user_id = message.from_user.id
+    user_message = message.text.strip()
+
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=telegram.ChatAction.TYPING)
+
     try:
-        # Get the user's question from the message
-        question = update.message.text.strip()
-
-        # Call the OpenAI API to get an answer
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=f"Q: {question}\nA:",
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0.7,
-        )
-
-        # Send the answer back to the user
-        answer = response.choices[0].text.strip()
-        # context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
-        status_msg.edit_text(answer)
+        response = handle_user_message(user_id, user_message, user_histories)
+        update.message.reply_text(response)
     except Exception as ex:
         print(ex)
-        status_msg.edit_text("'Due to a high volume of requests, I'm currently experiencing a heavy load, which may cause delays in my responses. I apologize for any inconvenience this may cause.\nPlease try again 🫠")
-        return
-
-def incoming_message_action(update, context):
-    message = update.message
-    context.dispatcher.run_async(answer_question, str(message.text), update, context)
-
+        update.message.reply_text(
+            "Due to a high volume of requests, I'm currently experiencing a heavy load, which may cause delays in my responses. I apologize for any inconvenience this may cause.\nPlease try again later.")
 
 
 def main() -> None:
@@ -73,14 +107,15 @@ def main() -> None:
 
     # Commands Listening
     dispatcher.add_handler(CommandHandler('start', start_handler, run_async=True))
-    dispatcher.add_handler(CommandHandler('start', about_handler, run_async=True))
     dispatcher.add_handler(CommandHandler('about', about_handler, run_async=True))
+    dispatcher.add_handler(CommandHandler('help', help_handler, run_async=True))
 
     # Message Incoming Action
     dispatcher.add_handler(MessageHandler(Filters.text, answer_question))
 
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
